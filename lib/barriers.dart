7798905216a0e857d73e2b198ea58f9bec79f4a3
Rehan Hamayun <rr8329203@gmail.com>
2022-02-17 03:44:35 +0500

    second commit

---
+++ lib/barriers.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Barriers extends StatelessWidget {
+  const Barriers({Key? key}) : super(key: key);
+
+  // IN PROGRESS
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/birddeath.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BirdDeath extends StatelessWidget {
+  const BirdDeath({Key? key}) : super(key: key);
+
+  // IN PROGRESS
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
